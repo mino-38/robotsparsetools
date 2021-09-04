7d3c2b6dcebf94d22c3bf4baa467ba6bd7a7b45c
minomushi <wngx5451@yahoo.co.jp>
2021-09-03 23:29:58 -0400

    First commit

---
+++ robotsparsetools/parse.py
@@ -0,0 +1,109 @@
+from urllib.request import urlopen
+from urllib.error import URLError
+from urllib.parse import urljoin, urlparse
+from itertools import product
+import re
+
+class error(BaseException):
+    class NotFoundError(Exception):
+        pass
+    
+    class NotURLError(Exception):
+        pass
+
+class Parse (dict):
+    def __init__(self, url, requests=False, **kwargs):
+        """
+        Parse robots.txt and returns a Parse instance.
+
+        >>> p = Parse("https://www.google.com/")
+        >>> {'*': ~~}
+        >>> Returns the Parse type of description for each User-Agent.
+        """
+        if not url.endswith("robots.txt"):
+            url = urljoin(url, "/robots.txt")
+        self._get_home(url)
+        result = request(url, use_requests=requests, option=kwargs)
+        data = parse(result.splitlines())
+        super().__init__(**data)
+
+    def _get_home(self, url):
+        parsed = urlparse(url)
+        if parsed.scheme:
+            self.home = f"{parsed.scheme}://{parsed.netloc}"
+        else:
+            raise error.NotURLError(f"'{url}' is not url")
+
+    def Allow(self, useragent="*"):
+        """
+        Get allow list from robots.txt.
+        If there was nothing, return None.
+        """
+        data = self.get(useragent)
+        if data:
+            return data.get("Allow")
+
+    def Disallow(self, useragent="*"):
+        """
+        Get disallow list from robots.txt.
+        If there was nothing, return None
+        """
+        data = self.get(useragent)
+        if data:
+            return data.get("Disallow")
+
+    def can_crawl(self, url, useragent="*"):
+        """
+        Returns True if crawl is allowed, False otherwise.
+        """
+        if not isinstance(url, str):
+            raise ValueError("'url' argument must give a string type")
+        if not url.startswith(self.home):
+            return True
+        disallow = self.Disallow(useragent)
+        allow = self.Allow(useragent) or ['']
+        link = url[len(self.home):]
+        if disallow:
+            for d, a in product(disallow, allow):
+                if link.startswith(a) or re.match(rf".*{a}.*", link):
+                    return True
+                if link.startswith(d) or re.match(rf".*{d}.*", link):
+                    return False
+        return True
+                
+
+def request(url, *, use_requests=False, option={}):
+    """
+    Send a request to robots.txt.
+    If use_requests argument is True, use requests module at the time of request
+    """
+    try:
+        if use_requests:
+            import requests
+            result = requests.get(url, **option).text
+        else:
+            with urlopen(url) as r:
+                result = r.read().decode()
+        return result
+    except URLError:
+        raise error.NotFoundError(f"'{url}' is not found")
+
+def parse(info):
+    """
+    Parse robots.txt
+    """
+    datas = {}
+    for i in info:
+        if not i or i.startswith("#"):
+            continue
+        if i.startswith("User-agent: "):
+            useragent = i[12:]
+            datas[useragent] = {}
+        else:
+            split = i.split(": ")
+            name = split[0]
+            data = split[1]
+            if name not in datas[useragent]:
+                datas[useragent][name] = []
+            datas[useragent][name].append(data)
+    return datas
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="robotsparsetools",
+    description="Parse robots.txt",
+    version="1.0.0",
+    author="minomushi",
+    url="https://github.com/mino-38/robotsparsetools",
+    packages=["robotsparsetools"],
+    licence="MIT"
+)
