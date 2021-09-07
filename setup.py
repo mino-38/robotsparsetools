0c370a6c04d2011d4cf0322a3cce1559c4172da2
minomushi <wngx5451@yahoo.co.jp>
2021-09-07 10:33:36 -0400

    バージョン更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Parse robots.txt",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="1.2.1",
+    version="1.2.2",
     author="minomushi",
     author_email="mino3@cocoro.uk",
     url="https://github.com/mino-38/robotsparsetools",
