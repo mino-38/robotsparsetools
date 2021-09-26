39ec9167755cde363d3c40671f9a40bf1f516677
minomushi <wngx5451@yahoo.co.jp>
2021-09-26 01:42:18 -0400

    -c/--crawl-delayオプションを追加

--- robotsparsetools/__main__.py
+++ robotsparsetools/__main__.py
@@ -8,23 +8,30 @@ def argument():
     parser = argparse.ArgumentParser(description="This is a command to check if crawls are allowed for URL.  Also, you can also get the Allow list and Disallow list by using the option")
     parser.add_argument("URL", help="URL you want to crawl")
     parser.add_argument("-u", "--user-agent", default="*", help="User-agent you want to check")
-    parser.add_argument("-a", "--allow", action="store_true")
-    parser.add_argument("-d", "--disallow", action="store_true")
+    parser.add_argument("-a", "--allow", action="store_true", help="Ouptut the Allow list")
+    parser.add_argument("-d", "--disallow", action="store_true", help="Output the Disallow list")
+    parser.add_argument("-c", "--crawl-delay", action="store_true", help="Output the Crawl-delay")
     return parser.parse_args()
 
 def main():
     args = argument()
     p = Parse(args.URL)
-    if not args.allow and not args.disallow:
+    if args.allow is args.disallow is args.crawl_delay is False:
         if p.can_crawl(args.URL, useragent=args.user_agent):
             print("Y")
         else:
             print("N")
     else:
         if args.allow:
-            print("\n".join(p.Allow(args.user_agent)))
+            allow = p.Allow(args.user_agent)
+            if allow:
+                print("\n".join(allow))
         if args.disallow:
-            print("\n".join(p.Disallow(args.user_agent)))
+            disallow = p.Disallow(args.user_agent)
+            if disallow:
+                print("\n".join(disallow))
+        if args.crawl_delay:
+            print(p.delay(args.user_agent))
 
 if __name__ == "__main__":
     main()
